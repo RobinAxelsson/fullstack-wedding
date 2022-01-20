84a9b3eb8299a2cf25a8346a7f3db2b7ff1e4ea5
RobinAxelsson <axelsson.robin@outlook.com>
2022-01-20 18:05:42 +0100

    added render unit tests

--- frontend/src/__test__/App.test.js
+++ frontend/src/__test__/App.test.js
@@ -1,8 +1,18 @@
-// import { render, screen } from '@testing-library/react';
-// import App from '../App';
+import { render, screen } from '@testing-library/react';
+import GuestForm from '../components/GuestForm';
 
-// test('renders Hello World', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/Hello World/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders Guest Form title english', () => {
+  render(<GuestForm language={"eng"}/>);
+  const linkElement = screen.getByText(/Join our Wedding/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders Guest Form title kurdi', () => {
+  render(<GuestForm language={"kurdi"}/>);
+  const linkElement = screen.getByText(/Bexerben bo/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders Guest Form svenska', () => {
+  render(<GuestForm language={"swe"}/>);
+  const linkElement = screen.getByText(/Fira våran dag!/i);
+  expect(linkElement).toBeInTheDocument();
+});
