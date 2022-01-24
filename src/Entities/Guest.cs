5124f549aebfaa55bcc6e66515ffee87b8d83399
RobinAxelsson <axelsson.robin@outlook.com>
2022-01-24 13:55:21 +0100

    added special food

--- src/Entities/Guest.cs
+++ src/Entities/Guest.cs
@@ -1,7 +1,7 @@
 using System.Text.Json;
 using Microsoft.Azure.Cosmos.Table;
 
-public record GuestDto(string Name, int Age, string MyPreferedLanguage, string ClosestTo, string[] Hobbies, string Relation);
+public record GuestDto(string Name, int Age, string MyPreferedLanguage, string ClosestTo, string Hobbies, string Relation, string SpecialFood);
 public class Guest : TableEntity
 {
     public Guest(){}
@@ -10,18 +10,20 @@ public class Guest : TableEntity
     public string MyPreferedLanguage { get; set; }
     public string ClosestTo { get; set; }
     public string Relation { get; set; }
-    public string[] Hobbies { get; set; }
+    public string Hobbies { get; set; }
+    public string SpecialFood { get; set; }
     public Guest(GuestDto guestDto)
     {
         RowKey = Guid.NewGuid().ToString("d");
-        PartitionKey = guestDto.MyPreferedLanguage ?? "english";
+        PartitionKey = guestDto.MyPreferedLanguage ?? "eng";
         Name = guestDto.Name;
         Age = guestDto.Age;
-        MyPreferedLanguage = guestDto.MyPreferedLanguage ?? "english";
+        MyPreferedLanguage = guestDto.MyPreferedLanguage ?? "eng";
         ClosestTo = guestDto.ClosestTo;
         Hobbies = guestDto.Hobbies;
         Relation = guestDto.Relation;
+        SpecialFood = guestDto.SpecialFood;
     }
-    public GuestDto ExtractDto() => new GuestDto(Name, Age, MyPreferedLanguage, ClosestTo, Hobbies, Relation);
+    public GuestDto ExtractDto() => new GuestDto(Name, Age, MyPreferedLanguage, ClosestTo, Hobbies, Relation, SpecialFood);
     public override string? ToString() => JsonSerializer.Serialize(this);
 }
